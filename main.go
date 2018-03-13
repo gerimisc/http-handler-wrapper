fa17f3cae4b9c74eadc98e828a84b6926a5ffce2
audis <audi.sugianto@gmail.com>
2018-03-13 21:01:13 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+type secretHandler struct{}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("auth")
+	if err == http.ErrNoCookie {
+		// not authenticated
+		w.Header().Set("Location", "/")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	}
+
+	if err != nil {
+		// some other error
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, success. call the next handler
+	h.next.ServeHTTP(w, r)
+}
+
+func mustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+func (s *secretHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Secret is 'secret'")
+}
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "welcome")
+	})
+
+	http.Handle("/secret", mustAuth(&secretHandler{}))
+
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
